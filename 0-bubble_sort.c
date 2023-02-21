e9f29c4700ce18c49ba77739020851624f9d703a
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-02-21 05:30:11 -0800

    Add task0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,7 +18,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size - 1 - i; j++)
 		{
-			if (array[j] > array [j + 1])
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
