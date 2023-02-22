440f91768e78d9c315dfaa33ab498d2d2e369334
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-02-21 18:35:48 -0800

    Add task2

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort a doulbly linked list of
+ * integers using Insertion sort
+ * @list: The list to sort.
+ *
+ * Return: void.
+ */
+void insertion_sort_list(listint_t **list)
+{
+
---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers using selection sort
+ * @array: array to sort.
+ * @size: Size of the array to sort.
+ *
+ * Return: void.
+ */
+void selection_sort(int *array, size_t size)
+{
+	int tmp = 0;
+	size_t i, j, p;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		p = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[p])
+				p = j;
+		}
+		if (p != i)
+		{
+			tmp = array[i];
+			array[i] = array[p];
+			array[p] = tmp;
+			print_array(array, size);	
+		}
+	}
+}
