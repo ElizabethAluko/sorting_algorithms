d02038e3f1ac83618f4c4d0dfa6597b00558b808
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-02-21 18:37:59 -0800

    Add task2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -29,7 +29,7 @@ void selection_sort(int *array, size_t size)
 			tmp = array[i];
 			array[i] = array[p];
 			array[p] = tmp;
-			print_array(array, size);	
+			print_array(array, size);
 		}
 	}
 }
